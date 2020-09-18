5a31a973f30e402353f163b24e6117746c3827c7
Andreas <andreas@code4thought.eu>
2020-09-18 09:37:06 +0300

    fix error

--- main.py
+++ main.py
@@ -61,15 +61,15 @@ except FileNotFoundError:
     idx_dict = make_test_set_idx(openml_datasets_ids, trained_models_dict)
 
 try:
-    mashap_scores_dict = joblib.load(f"cache/mashap_scores2.dict")
-    mashap_runtime_dict = joblib.load(f"cache/mashap_runtime2.dict")
+    mashap_scores_dict = joblib.load(f"cache/mashap_scores.dict")
+    mashap_runtime_dict = joblib.load(f"cache/mashap_runtime.dict")
 except FileNotFoundError:
-    print("========== CALCULATING MASHAP2 SCORES ==========")
+    print("========== CALCULATING MASHAP SCORES ==========")
     (mashap_scores_dict, mashap_runtime_dict,) = calculate_cache_scores(
         openml_datasets_ids, trained_models_dict, "mashap"
     )
-    joblib.dump(mashap_scores_dict, "cache/mashap_scores2.dict")
-    joblib.dump(mashap_runtime_dict, "cache/mashap_runtime2.dict")
+    joblib.dump(mashap_scores_dict, "cache/mashap_scores.dict")
+    joblib.dump(mashap_runtime_dict, "cache/mashap_runtime.dict")
 
 
 try:
@@ -86,14 +86,14 @@ except FileNotFoundError:
 ########################################################################
 
 try:
-    mashap_consistency_dict = joblib.load("cache/mashap_consistency2.dict")
+    mashap_consistency_dict = joblib.load("cache/mashap_consistency.dict")
 except FileNotFoundError:
-    print("========== CALCULATING MASHAP2 CONSISTENCY METRICS ==========")
-    mashap_scores_dict = joblib.load(f"cache/mashap_scores2.dict")
+    print("========== CALCULATING MASHAP CONSISTENCY METRICS ==========")
+    mashap_scores_dict = joblib.load(f"cache/mashap_scores.dict")
     mashap_consistency_dict = get_consistency_metrics(
         openml_datasets_ids, algorithm="mashap", scores_dict=mashap_scores_dict, idx_dict=idx_dict, trained_models_dict=trained_models_dict
     )
-    joblib.dump(mashap_consistency_dict, "cache/mashap_consistency2.dict")
+    joblib.dump(mashap_consistency_dict, "cache/mashap_consistency.dict")
 
 try:
     lime_consistency_dict = joblib.load("cache/lime_consistency.dict")
@@ -111,5 +111,5 @@ except FileNotFoundError:
 datasets = [ds for ds, v, t in openml_datasets_ids]
 
 runtime_calculations(mashap_runtime_dict, lime_runtime_dict, datasets)
-write_excel(mashap_consistency_dict, lime_consistency_dict, datasets, 'comparison_mashap_lime2')
-print(t_test('comparison_mashap_lime2.xls'))
+write_excel(mashap_consistency_dict, lime_consistency_dict, datasets, 'comparison_mashap_lime')
+print(t_test('comparison_mashap_lime.xls'))
